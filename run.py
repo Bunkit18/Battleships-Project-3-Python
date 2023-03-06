ed56dd8bf0abda8eba1207a3bd38c9fbd9009f82
Bunkit18 <jessswork18@gmail.com>
2023-03-06 21:30:34 +0000

    Added get_board_size that asks the user for a number between 2 and 3 to determine board size

--- run.py
+++ run.py
@@ -1,8 +1,28 @@
+def get_board_size():
+    """
+    Get the required size board from the user as int
+    """
+    print("You can play a 2x2 (easy) or 3x3 (hard) grid.")
+    print("Enter either '2' for 2x2 or '3' for 3x3\n")
+    squares = int(input("What size grid would you like?: "))
+    print("Validating entry...\n")
+    verified = verify_board_size(squares)
+
+
+def verify_board_size(squares):
+    """
+    Takes user input as a parameter and checks if the number received
+    is either '2' or '3'.
+    Otherwise, raises an exception and prints to terminal.
+    """
+
+
 def main():
     """
     The main function for the sudoku program
     Runs all program functions
     """
+    get_board_size()
 
 
 print("Welcome to Sudoku!\n")
